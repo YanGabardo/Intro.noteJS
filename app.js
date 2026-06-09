88cc17e32f751306ba796c8f2bc2ac25d6e8cad4
YanGabardo <yan.gabardo@gmail.com>
2026-06-09 13:38:07 -0300

    Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Internal Server Error\n');
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+    });
+});
+
+const port = 8080;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
